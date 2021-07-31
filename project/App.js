f1ccdd14e791a71b5a4c8619615b9f7fcb4d42b4
defwdahyun0 <wdh112139@gmail.com>
2021-07-31 17:28:09 +0900

    Upload Project

---
+++ project/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import MainPage from './pages/MainPage';
+import AboutPage from './pages/AboutPage';
+import DetailPage from './pages/DetailPage';
+
+export default function App(){
+  return (<MainPage/>)
+  // return (<DetailPage/>)
+}
