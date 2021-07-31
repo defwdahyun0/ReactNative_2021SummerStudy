3fc3f343b18879140bcf02f1ea39d56b6bf10fb9
defwdahyun0 <wdh112139@gmail.com>
2021-07-31 17:58:56 +0900

    state

--- project/pages/MainPage.js
+++ project/pages/MainPage.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import main from '../assets/main.png';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
 import data from '../data.json';
 import Card from '../components/Card';
 
+
 export default function MainPage() {
   console.disableYellowBox = true;
   //return 구문 밖에서는 슬래시 두개 방식으로 주석
 
-  let tip = data.tip;
-  let todayWeather = 10 + 17;
-  let todayCondition = "흐림"
+	//useState 사용법
+	//[state,setState] 에서 state는 이 컴포넌트에서 관리될 상태 데이터를 담고 있는 변수
+  //setState는 state를 변경시킬때 사용해야하는 함수
 
+  //모두 다 useState가 선물해줌
+  //useState()안에 전달되는 값은 state 초기값
+  const [state,setState] = useState([])
+	
+
+	//하단의 return 문이 실행되어 화면이 그려진다음 실행되는 useEffect 함수
+  //내부에서 data.json으로 부터 가져온 데이터를 state 상태에 담고 있음
+  useEffect(()=>{
+    setState(data)
+  },[])
+
+//   let tip = data.tip;
+	//data.json 데이터는 state에 담기므로 상태에서 꺼내옴
+  let tip = state.tip;
+	let todayWeather = 10 + 17;
+  let todayCondition = "흐림"
   return (
     /*
       return 구문 안에서는 {슬래시 + * 방식으로 주석
     */
     <ScrollView style={styles.container}>
       <Text style={styles.title}>나만의 꿀팁</Text>
-      <Text style={styles.weather}>오늘의 날씨: {todayWeather + '°C ' + todayCondition} </Text>
+			 <Text style={styles.weather}>오늘의 날씨: {todayWeather + '°C ' + todayCondition} </Text>
       <Image style={styles.mainImage} source={main}/>
       <ScrollView style={styles.middleContainer} horizontal indicatorStyle={"white"}>
         <TouchableOpacity style={styles.middleButton01}><Text style={styles.middleButtonText}>생활</Text></TouchableOpacity>
@@ -33,23 +50,9 @@ export default function MainPage() {
             return (<Card content={content} key={i}/>)
           })
         }
+        
       </View>
-      {/*
-      <View style={styles.cardContainer}>
-         { 
-          tip.map((content,i)=>{ // 반복문
-            return (<View style={styles.card} key={i}>
-              <Image style={styles.cardImage} source={{uri:content.image}}/>
-              <View style={styles.cardText}>
-                <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
-                <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
-                <Text style={styles.cardDate}>{content.date}</Text>
-              </View>
-            </View>)
-          })
-         }
-      </View>
-      */}
+   
     </ScrollView>
   );
 }
@@ -69,7 +72,7 @@ const styles = StyleSheet.create({
     //왼쪽 공간으로 부터 이격
     marginLeft:20
   },
-  weather:{
+ weather:{
     alignSelf:"flex-end",
     paddingRight:20
   },
@@ -86,7 +89,6 @@ const styles = StyleSheet.create({
     alignSelf:"center"
   },
   middleContainer:{
-    // borderWidth:1, 영역을 눈으로 볼 수 있게끔 한다.
     marginTop:20,
     marginLeft:10,
     height:60
@@ -119,7 +121,7 @@ const styles = StyleSheet.create({
   middleButtonText: {
     color:"#fff",
     fontWeight:"700",
-    //텍스트의 현재 위치에서의 정렬 // 중요! 
+    //텍스트의 현재 위치에서의 정렬
     textAlign:"center"
   },
   middleButton04: {
@@ -133,39 +135,6 @@ const styles = StyleSheet.create({
   cardContainer: {
     marginTop:10,
     marginLeft:10
-  },
-  card:{
-    flex:1,
-    //컨텐츠들을 가로로 나열
-    //세로로 나열은 column <- 디폴트 값임 
-    flexDirection:"row",
-    margin:10,
-    borderBottomWidth:0.5,
-    borderBottomColor:"#eee",
-    paddingBottom:10
-
-  },
-  cardImage: {
-    flex:1,
-    width:100,
-    height:100,
-    borderRadius:10,
-  },
-  cardText: {
-    flex:2,
-    flexDirection:"column",
-    marginLeft:10,
-  },
-  cardTitle: {
-    fontSize:20,
-    fontWeight:"700"
-  },
-  cardDesc: {
-    fontSize:15
-  },
-  cardDate: {
-    fontSize:10,
-    color:"#A6A6A6",
   }
-  
+
 });
