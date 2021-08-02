fa6b1438305bbcb8888e9c598409e40cf66eb9bb
defwdahyun0 <wdh112139@gmail.com>
2021-08-02 17:59:17 +0900

    jsx example

--- jsx/14_main.js
+++ jsx/14_main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import main from './assets/main.png';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
 
 export default function App() {
